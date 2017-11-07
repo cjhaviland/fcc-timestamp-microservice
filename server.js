d76f2fda35eb753b946bab6cce2677a511362663
Glitch (elderly-innocent) <none>
2017-11-07 18:59:31 +0000

    Exporting to cjhaviland/fcc-timestamp-microservice

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:date', (req, res) => {
-  let unixDate = new Date(req.params.date);
+  let unixDate = new Date(Number(req.params.date));
   let natDate = chrono.parseDate(req.params.date);
   
   if (unixDate.isValid()) {
